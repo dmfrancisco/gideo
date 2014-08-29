8b643753ca24160c222de5fe82605b18acf9097d
David Francisco <dmfrancisc@gmail.com>
2014-08-29 19:59:35 +0100

    Use video.js with its flash fallback
    Mediaelement.js may be a better choice, but for now this will do.

--- embed.js
+++ embed.js
@@ -1,55 +1,72 @@
-if (!window.GIDEO) {
-  window.GIDEO = true;
+(function () {
+  if (!window.GIDEO) {
+    window.GIDEO = true;
 
-  function listen(eventName, elem, func) {
-    if (elem.addEventListener) {
-      elem.addEventListener(eventName, func, false);
+    function listen(eventName, elem, func) {
+      if (elem.addEventListener) {
+        elem.addEventListener(eventName, func, false);
+      }
+      else if (elem.attachEvent) {
+        elem.attachEvent("on" + eventName, func);
+      }
     }
-    else if (elem.attachEvent) {
-      elem.attachEvent("on" + eventName, func);
-    }
-  }
 
-  function isVisible(elem) {
-    var rect = elem.getBoundingClientRect();
+    function isVisible(elem) {
+      var rect = elem.getBoundingClientRect();
 
-    return (
+      return (
         rect.top > -elem.offsetHeight &&
         rect.top < (window.innerHeight || document.documentElement.clientHeight)
-    );
+      );
+    }
+
+    window.setInterval(function () {
+      var embeds = document.querySelectorAll('.gideo');
+
+      for (var i = 0; i < embeds.length; i++) {
+        var gideo = embeds[i].player;
+
+        if (!gideo) {
+          continue;
+        } else if (isVisible(embeds[i])) {
+          if (gideo.paused()) {
+            try { gideo.currentTime(0); }
+            catch (e) { /* Random error with the flash fallback */ }
+            gideo.play();
+          }
+        } else if (!gideo.paused()) {
+          gideo.pause();
+        }
+      }
+    }, 100);
+
+    listen("click", document.querySelector('body'), function (e) {
+      if (e.target.className === "gideo-mute") {
+        var gideo = e.target.nextSibling.player;
+
+        if (!gideo) {
+          return;
+        } else if (gideo.muted()) {
+          try { gideo.currentTime(0); }
+          catch (e) { /* Random error with the flash fallback */ }
+
+          gideo.muted(false);
+          e.target.style.background = "url('/on.png')";
+        } else {
+          gideo.muted(true);
+          e.target.style.background = "url('/off.png')";
+        }
+      }
+    });
   }
 
-  window.setInterval(function () {
-    var embeds = document.querySelectorAll('.gideo');
+  // Force mute in browsers with flash fallback
+  var embeds = document.querySelectorAll('.gideo'),
+    mute = function () { this.muted(true); }
 
-    for (var i = 0; i < embeds.length; i++) {
-      var gideo = embeds[i];
-
-      if (isVisible(gideo)) {
-        if (gideo.paused === true) {
-          try { gideo.currentTime = 0 }
-          catch (e) { /* Not loaded yet */ }
-          gideo.play();
-        }
-      } else if (!embeds[i].paused) {
-        gideo.pause();
-      }
+  for (var i = 0; i < embeds.length; i++) {
+    if (!embeds[i].player) {
+      videojs(embeds[i]).ready(mute);
     }
-  }, 100);
-
-  listen("click", document.querySelector('body'), function (e) {
-    if (e.target.className === "gideo-mute") {
-      var gideo = e.target.nextSibling;
-
-      if (gideo.muted === true) {
-        gideo.muted = false;
-        try { gideo.currentTime = 0 }
-        catch (e) { /* Not loaded yet */ }
-        e.target.style.background = "url('/on.png')";
-      } else {
-        gideo.muted = true;
-        e.target.style.background = "url('/off.png')";
-      }
-    }
-  });
-}
+  }
+})();
