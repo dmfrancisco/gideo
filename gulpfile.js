fac0154559ff335aeb71f4afb231ab41c1085861
David Francisco <dmfrancisc@gmail.com>
2014-09-03 14:57:24 +0100

    Minor refactoring done to Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,65 @@
 var gulp = require('gulp');
+var header = require('gulp-header');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var info = require('./package.json');
+var pkg = require('./package.json');
 
-var settings = {
-  assets: [
-    './node_modules/video.js/dist/video-js/video-js.swf'
-  ],
-  vendorScripts: [
-    './node_modules/video.js/dist/video-js/video.js'
-  ],
-  appScripts: [
-    './embed.js'
-  ],
-  appScript: 'embed.min.js',
-  prodScript: ('gideo-'+ info.version +'.min.js')
+var config, version = pkg.version.split('.');
+
+version = {
+  major: version[0],
+  minor: version[1],
+  patch: version[2],
+  majorMinor: version[0] + '.' + version[1]
+};
+
+config = {
+  vendor: {
+    assets: [
+      './node_modules/video.js/dist/video-js/video-js.swf'
+    ],
+    scripts: [
+      './node_modules/video.js/dist/video-js/video.js'
+    ],
+  },
+  build: {
+    src: [
+      './embed.js'
+    ],
+    dest: 'embed.min.js'
+  },
+  dist: ('gideo-'+ version.majorMinor +'.min.js'),
+  banner: "/** <%= pkg.name %> v<%= pkg.version %> */\n"
 }
 
-// Copy vendor assets to the root folder
+// Copy vendor assets
 gulp.task('copy-assets', function () {
-  return gulp.src(settings.assets)
+  return gulp.src(config.vendor.assets)
     .pipe(gulp.dest('.'));
 });
 
 // Minify and concatenate application scripts
-gulp.task('build-app', function() {
-  return gulp.src(settings.appScripts)
+gulp.task('build', function() {
+  return gulp.src(config.build.src)
     .pipe(uglify())
-    .pipe(concat(settings.appScript))
+    .pipe(concat(config.build.dest))
     .pipe(gulp.dest('.'));
 });
 
 // Concatenate application and vendor scripts
-gulp.task('build-prod', ['build-app'], function() {
+gulp.task('dist', ['build'], function() {
   return gulp.src(
-      settings.vendorScripts.concat([ settings.appScript ])
+      config.vendor.scripts.concat([ config.build.dest ])
     )
-    .pipe(concat(settings.prodScript))
+    .pipe(concat(config.dist))
+    .pipe(header(config.banner, { pkg: pkg }))
     .pipe(gulp.dest('.'));
 });
 
 // Rerun the tasks when a file changes
 gulp.task('watch', ['default'], function(fn) {
-  gulp.watch(settings.appScripts, ['build-prod']);
+  gulp.watch(config.build.src, ['dist']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['copy-assets', 'build-prod']);
+gulp.task('default', ['copy-assets', 'dist']);
